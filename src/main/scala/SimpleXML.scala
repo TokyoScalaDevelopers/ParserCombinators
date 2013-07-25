1160e04f00ceecc89a3d0e4adca68a98d82e6f36
Devon Stewart <blast@hardchee.se>
2013-07-25 16:09:18 +0900

    Starting to add support for self-closing tags

--- src/main/scala/SimpleXML.scala
+++ src/main/scala/SimpleXML.scala
@@ -5,13 +5,18 @@ import scala.util.parsing.combinator.RegexParsers
 object XMLParser extends RegexParsers {
     val identifier = "test"
     val attributes = "foo=\"bar\""
-    val tag = "<" ~> (identifier ~ rep(attributes)) <~ ">"
 
-    def handleString(xml: String) = parseAll(tag, xml)
+    val selfclosingtag = ( "<" ) ~> (identifier ~ rep(attributes)) <~ ( "/" ~ ">")
+    val opentag = "<" ~> (identifier ~ rep(attributes)) <~ ">"
+    val closetag = "<" ~> (identifier ~ rep(attributes)) <~ ">"
+
+    val element: Parser[Any] = selfclosingtag | ( opentag ~ element ~ closetag )
+
+    def handleString(xml: String) = parseAll(element, xml)
 }
 
 object SimpleXML {
     def main(args: Array[String]) {
-        println(XMLParser.handleString("<test>"))
+        println(XMLParser.handleString("<test / >"))
     }
 }
