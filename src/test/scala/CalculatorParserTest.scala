7153a74a3be5ee46e68845837a7378dda76639bc
Devon Stewart <blast@hardchee.se>
2013-08-21 21:35:13 +0900

    Refactoring tests to be less all over the place

--- src/test/scala/CalculatorParserTest.scala
+++ src/test/scala/CalculatorParserTest.scala
@@ -4,14 +4,10 @@ import se.hardchee.CalculatorParser._
 
 object CalculatorParserTest {
     def parse(expr: String) = {
-        println("Parsing: " + expr)
         CalculatorParser.parseExpression(expr)
     }
     def evaluate(expr: Component) = {
-        println("Evaluating: " + expr)
-        val r = CalculatorEvaluator.evaluate(expr)
-        println("Result: " + r)
-        r
+        CalculatorEvaluator.evaluate(expr)
     }
 
     def calc(arg: Any) = arg match {
@@ -21,15 +17,18 @@ object CalculatorParserTest {
     }
 
     def main(args: Array[String]) {
-        goodExpression
-        bogusExpression
-    }
+      val expressions = List(
+        ("good expression", "1 - 2 + 3 * (4 + 5)", Some(Number(26.0))),
+        ("bad expression", Terms(List( Number(1.0), Operator("*") )), None)
+      )
 
-    def goodExpression {
-        calc("1 - 2 + 3 * (4 + 5)")
-    }
-
-    def bogusExpression {
-        calc(Terms(List( Number(1.0), Operator("*") )))
+      expressions.map({ case (message, expr: Any, expectedResult: Option[Number]) =>
+        println("--------------------")
+        println("Testing \"%s\": %s".format(message, expr))
+        val result = calc(expr)
+        val expectedMessage = "%s == %s: %s".format(result.toString, expectedResult.toString, (result == expectedResult).toString )
+        println("  Result: %s".format(expectedMessage))
+      })
+      println("--------------------")
     }
 }
